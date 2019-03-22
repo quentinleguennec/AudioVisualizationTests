2422e2ac3b1d71a5e30c92a1f2661aae0302bf76
Quentin Le Guennec <quentin@tengio.com>
2019-03-22 09:58:09 +0000

    Initial commit.

---
+++ Assets/Scripts/AudioPeer.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+
+public class AudioPeer : Singleton<AudioPeer>
+{
+    [SerializeField]
+    private FFTWindow fftWindowMode = FFTWindow.Blackman;
+
+    public float[] Samples { get; private set; } = new float[GameMaster.SamplesCount];
+    public float[] FrequencyBands { get; private set; } = new float[GameMaster.FrequencyBandsCount];
+
+    private AudioSource audioSource;
+
+
+    protected AudioPeer() { }
+
+    private void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    private void Update()
+    {
+        audioSource.GetSpectrumData(Samples, 0, fftWindowMode);
+    }
+
+    private void GetFrequencyBands()
+    {
+        int count = 0;
+        for (int i = 0; i < GameMaster.FrequencyBandsCount; i++)
+        {
+            float average = 0;
+            int samplesCount = (int)Mathf.Pow(2, i + 1);
+            if(i == 7)
+            {
+                samplesCount += 2;
+            }
+            for (int j = 0;j  < samplesCount; j++)
+            {
+                average += Samples[count] * (count + 1);
+                count ++;
+            }
+
+            average /= count;
+
+            FrequencyBands[i] = average * 10f;
+
+        }
+
+    }
+}
---
+++ Assets/Scripts/GameMaster.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+public class GameMaster : Singleton<GameMaster>
+{
+    public const int SamplesCount = 512;
+    public const int FrequencyBandsCount = 8;
+
+    [SerializeField]
+    private GameObject sampleCubePrefab;
+    [SerializeField]
+    private Transform sampleCubesParent;
+    [SerializeField]
+    private float maxCubeHeight;
+
+    private GameObject[] sampleCubes = new GameObject[SamplesCount];
+
+
+    protected GameMaster() { }
+
+    void Start()
+    {
+        for (int i = 0; i < SamplesCount; i++)
+        {
+            GameObject sampleCube = Instantiate(sampleCubePrefab, sampleCubesParent);
+            sampleCube.name = "SampleCube" + i;
+            sampleCube.transform.eulerAngles = new Vector3(0, i * (SamplesCount / 360), 0);
+            sampleCube.transform.position = sampleCube.transform.forward * 100;
+            sampleCubes[i] = sampleCube;
+        }
+    }
+
+    void Update()
+    {
+        for (int i = 0; i < SamplesCount; i++)
+        {
+            if (sampleCubes != null)
+            {
+                sampleCubes[i].transform.localScale = new Vector3(1, 2 + AudioPeer.Instance.Samples[i] * maxCubeHeight, 1);
+                sampleCubes[i].transform.position = new Vector3(sampleCubes[i].transform.position.x, 0.5f * (2 + AudioPeer.Instance.Samples[i] * maxCubeHeight), sampleCubes[i].transform.position.z);
+            }
+        }
+    }
+}
